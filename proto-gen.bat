8e758bb30851bfd8f6ff6d3c65b1280f429c54b3
Zeb Fross <zebfross@hotmail.com>
2022-11-02 20:12:38 -0700

    fix structs.proto

--- proto-gen.bat
+++ proto-gen.bat
@@ -1,8 +1,10 @@
 cd proto
-protoc -I .. -I . --go_out=paths=source_relative:../syncthing/ protocol\bep.proto protocol\deviceid_test.proto
-protoc -I .. -I . --go_out=paths=source_relative:../syncthing/ protocol\bep.proto protocol\deviceid_test.proto
-protoc -I .. -I . --go_out=paths=source_relative:../syncthing/ config\authmode.proto config\blockpullorder.proto config\config.proto config\deviceconfiguration.proto config\folderconfiguration.proto config\foldertype.proto config\guiconfiguration.proto config\ldapconfiguration.proto config\ldaptransport.proto config\observed.proto config\optionsconfiguration.proto config\pullorder.proto config\size.proto config\tuning.proto config\versioningconfiguration.proto
-protoc -I .. -I . --go_out=paths=source_relative:../syncthing/ fs\copyrangemethod.proto fs\types.proto
-protoc -I .. -I . --go_out=paths=source_relative:../syncthing/ db\structs.proto
-protoc -I .. -I . --go_out=paths=source_relative:../syncthing/ discover\local.proto
+set GOPATH=--go_out=paths=source_relative:../syncthing/
+
+protoc -I .. -I . %GOPATH% protocol\bep.proto protocol\deviceid_test.proto
+protoc -I .. -I . %GOPATH% protocol\bep.proto protocol\deviceid_test.proto
+protoc -I .. -I . %GOPATH% config\authmode.proto config\blockpullorder.proto config\config.proto config\deviceconfiguration.proto config\folderconfiguration.proto config\foldertype.proto config\guiconfiguration.proto config\ldapconfiguration.proto config\ldaptransport.proto config\observed.proto config\optionsconfiguration.proto config\pullorder.proto config\size.proto config\tuning.proto config\versioningconfiguration.proto
+protoc -I .. -I . %GOPATH% fs\copyrangemethod.proto fs\types.proto
+protoc -I .. -I . %GOPATH% db\structs.proto
+protoc -I .. -I . %GOPATH% discover\local.proto
 cd ..
